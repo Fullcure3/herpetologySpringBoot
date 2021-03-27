d51247fc36dcbfffae3d1dac85e39762ef8d071b
Christian Soto <christiansotogw2@gmail.com>
2021-03-27 16:44:02 -0400

    Implementing Spring security configuration

--- src/main/java/org/portfolio/controllers/ClassificationRestController.java
+++ src/main/java/org/portfolio/controllers/ClassificationRestController.java
@@ -1,13 +1,14 @@
 package org.portfolio.controllers;
 
-import org.portfolio.page.ClassificationViewPage;
 import org.portfolio.services.ClassificationService;
 import org.portfolio.views.ClassificationView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/classifications")
---
+++ src/main/java/org/portfolio/security/ApplicationSecurityConfig.java
@@ -0,0 +1,52 @@
+package org.portfolio.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "index", "/api/genus/all")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        var userDetailsChris = User.builder()
+                .username("Chris")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        var userDetailsGuest = User.builder()
+                .username("guest")
+                .password(passwordEncoder.encode("temp"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(userDetailsChris, userDetailsGuest);
+    }
+}
